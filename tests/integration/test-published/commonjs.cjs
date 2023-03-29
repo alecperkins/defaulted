45e201f082fdb20fdc91ac0939a4fd243179f63f
Alec Perkins <mail@alecperkins.me>
2023-03-29 00:37:54 -0400

    chore: Integration tests

--- tests/integration/test-published/commonjs.cjs
+++ tests/integration/test-published/commonjs.cjs
@@ -1,4 +1,22 @@
-
 const conf = require("defaulted");
 
-conf();
+const config = conf({
+  ENVIRONMENT: 'local',
+  MY_HOST: "example.com",
+  PORT: 1234,
+}, {
+  prod: {
+    PORT: 80,
+  },
+  test: {
+    PORT: 8080,
+  },
+});
+
+if (
+  config.MY_HOST !== "example.com"
+  || config.PORT !== 8080
+) {
+  process.exit(1);
+}
+
--- tests/integration/test-published/ecma.mjs
+++ tests/integration/test-published/ecma.mjs
@@ -1,3 +1,22 @@
 import conf from "defaulted";
 
-conf();
+const config = conf({
+  ENVIRONMENT: 'local',
+  MY_HOST: "example.com",
+  PORT: 1234,
+}, {
+  prod: {
+    PORT: 80,
+  },
+  test: {
+    PORT: 8080,
+  },
+});
+
+if (
+  config.MY_HOST !== "example.com"
+  || config.PORT !== 8080
+) {
+  process.exit(1);
+}
+
