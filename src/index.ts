fd2313fa4c6189b25812aa909142682bfaf4ac83
Alec Perkins <mail@alecperkins.me>
2023-03-28 23:55:18 -0400

    feat: Merge env with defaults, overrides

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-
 /**
+ * Creates a typed object for configuration, requiring the definition of
+ * defaults and allowing for per-environment values and overrides via process.env.
  * 
+ * 
+ * @param defaults The default configuration values.
+ * @param environment_overrides set or unset defaults for an explicit process.env.ENVIRONMENT
+ * @returns the config combined from defaults, overrides and process.env
  */
-export default function conf () {
-  
+const conf: (
+  <T extends {}>(defaults: T, environment_overrides?: { [env: string]: Partial<T> }) => T
+) = function conf (defaults, environment_overrides) {
+
+  const config: typeof defaults = {...defaults};
+
+  if (environment_overrides && process.env.ENVIRONMENT) {
+    Object.assign(config, environment_overrides[process.env.ENVIRONMENT] ?? {})
+  }
+
+  Object.keys(config).forEach((key) => {
+    const _key = key as keyof typeof defaults;
+    const override = process.env[_key as string];
+    if (typeof override === "string") {
+      (config as any)[_key] = override;
+    }
+  });
+
+  return config;
 }
+
+export default conf;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,10 +3,60 @@ import { describe, expect, test } from "vitest";
 
 import conf from '../src';
 
+function overrideEnv (obj: { [key: string]: string }) {
+  const orig = { ...process.env };
+  Object.assign(process.env, obj);
+  return () => {
+    Object.keys(obj).forEach((key) => {
+      delete process.env[key];
+    });
+    Object.assign(process.env, orig);
+  };
+}
+
 describe("conf", () => {
 
-  test("does X", async () => {
-    expect(true).toEqual(false);
+  test("passes values through", async () => {
+    const config = conf({ MYVAL: "testing" });
+    expect(config.MYVAL).toEqual("testing");
+  });
+
+  test("reads from environment", async () => {
+    const reset = overrideEnv({ MYVAL: "different" });
+    const config = conf({ MYVAL: "testing" });
+    expect(config.MYVAL).toEqual("different");
+    reset();
+  });
+
+  test("reads from environment despite ENVIRONMENT", async () => {
+    const reset = overrideEnv({
+      ENVIRONMENT: "test",
+      MYVAL: "different",
+    });
+    const config = conf({
+      MYVAL: "testing",
+    }, {
+      test: {
+        MYVAL: "test override",
+      },
+    });
+    expect(config.MYVAL).toEqual("different");
+    reset();
+  });
+
+  test("switches defaults on ENVIRONMENT", async () => {
+    const reset = overrideEnv({
+      ENVIRONMENT: "test",
+    });
+    const config = conf({
+      MYVAL: "testing",
+    }, {
+      test: {
+        MYVAL: "test override",
+      },
+    });
+    expect(config.MYVAL).toEqual("test override");
+    reset();
   });
 
 });
