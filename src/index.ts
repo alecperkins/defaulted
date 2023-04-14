84f771f6ca0d2453ff099d0dc19666237de63bc4
Alec Perkins <mail@alecperkins.me>
2023-04-14 19:39:59 -0400

    chore: Deduplicate internals

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-/**
- * Creates a typed object for configuration, requiring the definition of
- * defaults and allowing for per-environment values and overrides via process.env.
- * 
- * @param defaults - The default configuration values.
- * @param overrides - set or unset defaults for an explicit process.env.ENVIRONMENT
- * @returns the config combined from defaults, overrides and process.env
- */
-const defaulted: (
-  <T extends {}>(defaults: T, overrides?: { [env: string]: Partial<T> }) => { readonly [key in keyof T]: T[key] } & { ENVIRONMENT: string | undefined }
-) & { secrets: typeof secrets } = function defaulted (defaults, overrides) {
+
+function _makeConfig <T extends {}> (args: {
+  defaults: T;
+  expected_keys: Set<keyof T>;
+  overrides: { [env: string]: Partial<T> };
+  is_secret: boolean;
+  include_ENVIRONMENT: boolean;
+}) {
+
+  const {
+    defaults,
+    expected_keys,
+    overrides,
+    is_secret,
+    include_ENVIRONMENT,
+  } = args;
 
   const actual_config: typeof defaults = {
     ...defaults,
   };
 
-  const expected_keys = new Set(Object.keys(actual_config));
   const unexpected_keys = new Set();
   const missing_keys = new Set();
   if (overrides) {
     Object.values(overrides).forEach(override => {
       Object.keys(override).forEach(key => {
-        if (!expected_keys.has(key)) {
+        if (!expected_keys.has(key as any)) {
           unexpected_keys.add(key);
         }
       });
@@ -73,29 +77,55 @@ const defaulted: (
   });
   
   if (unexpected_keys.size > 0) {
-    throw new Error(`Unexpected keys in overrides: "${ Array.from(unexpected_keys).join('","') }"`);
+    throw new Error(`Unexpected ${ is_secret ? "secret " : "" }keys in overrides: "${ Array.from(unexpected_keys).join('","') }"`);
   }
   if (missing_keys.size > 0) {
-    throw new Error(`Required keys not present in env: "${ Array.from(missing_keys).join('","') }"`);
+    throw new Error(`Required ${ is_secret ? "secret " : "" }keys not present in env: "${ Array.from(missing_keys).join('","') }"`);
   }
 
-  // Set this last so it doesn't get caught in validation.
-  Object.assign(actual_config, {
-    ENVIRONMENT: process.env.ENVIRONMENT,
-  });
+  if (include_ENVIRONMENT) {
+    // Set this last so it doesn't get caught in validation.
+    Object.assign(actual_config, {
+      ENVIRONMENT: process.env.ENVIRONMENT,
+    });
+  }
 
   const config = new Proxy(actual_config, {
     get: (orig, key) => {
       if ((Object as any).hasOwn(orig, key)) {
       return orig[key];
       }
-      throw new Error(`Cannot read unspecified property on config: "${ String(key) }"`);
+      throw new Error(`Cannot read unspecified property on ${ is_secret ? "secrets" : "config" }: "${ String(key) }"`);
     },
     set: (orig, key, val) => {
-      throw new Error(`Cannot assign to read only property on config: "${ String(key) }"`);
+      throw new Error(`Cannot assign to read only property on ${ is_secret ? "secrets" : "config" }: "${ String(key) }"`);
     },
   });
   return config as any;
+}
+
+/**
+ * Creates a typed object for configuration, requiring the definition of
+ * defaults and allowing for per-environment values and overrides via process.env.
+ * 
+ * @param defaults - The default configuration values.
+ * @param overrides? - set or unset defaults for an explicit process.env.ENVIRONMENT
+ * @returns the config combined from defaults, overrides and process.env
+ */
+const defaulted: (
+  <T extends {}>(
+    defaults: T,
+    overrides?: { [env: string]: Partial<T> }
+  ) => { readonly [K in keyof T]: T[K] }
+  & { ENVIRONMENT: string | undefined }
+) & { secrets: typeof secrets } = function defaulted (defaults, overrides = {}) {
+  return _makeConfig({
+    defaults,
+    expected_keys: new Set(Object.keys(defaults) as Array<keyof typeof defaults>),
+    overrides,
+    is_secret: false,
+    include_ENVIRONMENT: true,
+  });
 }
 
 /**
@@ -114,55 +144,23 @@ const defaulted: (
  */
 function secrets <T extends ReadonlyArray<string>> (
   keys: T,
-  overrides?: { [env: string]: { [key in T[number]]?: string } }
-): { readonly [key in T[number]]: string } {
-  const actual_secrets = {} as {
-    [key in T[number]]: string;
+  overrides: { [env: string]: { [K in T[number]]?: string } } = {}
+): { readonly [K in T[number]]: string } {
+
+  const defaults = {} as {
+    [K in T[number]]: string;
   };
-  const expected_keys = new Set(keys);
-  const unexpected_keys = new Set();
-  const missing_keys = new Set();
-
-  if (overrides) {
-    Object.values(overrides).forEach(override => {
-      Object.keys(override).forEach(key => {
-        if (!expected_keys.has(key)) {
-          unexpected_keys.add(key);
-        }
-      });
-    });
-  }
-
-  if (overrides && process.env.ENVIRONMENT) {
-    Object.assign(actual_secrets, overrides[process.env.ENVIRONMENT] ?? {})
-  }
-
   keys.forEach(key => {
-    actual_secrets[key] = process.env[key] ?? actual_secrets[key];
-    if (typeof actual_secrets[key] === "undefined") {
-      missing_keys.add(key);
-    }
+    Object.defineProperty(defaults, key, { enumerable: true, value: undefined });
   });
 
-  if (unexpected_keys.size > 0) {
-    throw new Error(`Unexpected secret keys in overrides: "${ Array.from(unexpected_keys).join('","') }"`);
-  }
-  if (missing_keys.size > 0) {
-    throw new Error(`Required secret keys not present in env: "${ Array.from(missing_keys).join('","') }"`);
-  }
-
-  const _secrets = new Proxy(actual_secrets, {
-    get: (orig, key) => {
-      if ((Object as any).hasOwn(orig, key)) {
-      return orig[key];
-      }
-      throw new Error(`Cannot read unspecified property on secrets: "${ String(key) }"`);
-    },
-    set: (orig, key, val) => {
-      throw new Error(`Cannot assign to read only property on secrets: "${ String(key) }"`);
-    },
+  return _makeConfig({
+    defaults,
+    expected_keys: new Set(keys),
+    overrides,
+    is_secret: true,
+    include_ENVIRONMENT: false,
   });
-  return _secrets as { readonly [key in T[number]]: string };
 }
 defaulted.secrets = secrets;
 export default defaulted;
